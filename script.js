390617f3e8ebb8ede1096fc344835ea05c1d0867
Vladimir Nechaev <nechadimir@gmail.com>
2023-11-02 16:14:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollToBottomButton = document.getElementById('scroll-to-bottom');
+
+    scrollToBottomButton.addEventListener('click', function () {
+        const documentHeight = document.documentElement.scrollHeight;
+        window.scrollTo({
+            top: documentHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+});
