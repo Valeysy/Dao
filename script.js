7af9374e51f3b9632bcb43b3b7731a98c425abe4
Vladimir Nechaev <nechadimir@gmail.com>
2023-11-02 17:58:20 +0100

    Major animation update

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollToBottomButton = document.getElementById('scroll-to-bottom');
+
+    scrollToBottomButton.addEventListener('click', function () {
+        const documentHeight = document.documentElement.scrollHeight;
+        window.scrollTo({
+            top: documentHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+});
